bc5d1dd75800dc56356c36b1f6e892a630489183
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2024-04-13 07:09:19 -0400

    more challengues

--- challengues/question5.7.js
+++ challengues/question5.7.js
@@ -1,27 +1,28 @@
 // Implemente retryRequest function that executes the getUserInfo function for nrOfTimes times until it succeeds.
 async function retryRequest(promiseFunc, nrOfRetries) {
-    // Write your code here
-    let promiseArr = [];
-    for (let i = 0; i < nrOfRetries; i++) {
-        promiseArr.push(promiseFunc())
-    }
-    return Promise.race(promiseArr);
+  // Write your code here
+  let promiseArr = [];
+  for (let i = 0; i < nrOfRetries; i++) {
+    promiseArr.push(promiseFunc());
+  }
+  return Promise.race(promiseArr);
 }
-        
+
 let hasFailed = false;
 function getUserInfo() {
-return new Promise((resolve, reject) => {
-    if(!hasFailed) {
-    hasFailed = true;
-    reject("Exception!");
+  return new Promise((resolve, reject) => {
+    if (!hasFailed) {
+      hasFailed = true;
+      reject("Exception!");
     } else {
-    resolve("Fetched user!");
+      resolve("Fetched user!");
     }
-});
+  });
 }
 let promise = retryRequest(getUserInfo, 3);
-if(promise) {
-promise.then((result) => console.log(result))
-.catch((error) => console.log("Error!"));
+if (promise) {
+  promise
+    .then((result) => console.log(result))
+    .catch((error) => console.log("Error!"));
 }
 module.exports.retryRequest = retryRequest;
